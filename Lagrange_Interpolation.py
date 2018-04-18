9328bf7a0b545c350724e876628611faa328adee
lanzili <794514174@qq.com>
2018-04-18 23:20:13 +0800

    new file

---
+++ Lagrange_Interpolation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__author__ = lanzili
+__mtime__ = '2018/4/18'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+             ┏┓   ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃   ☃    ┃
+            ┃ ┳┛  ┗┳ ┃
+            ┃   ┻    ┃
+            ┗━┓    ┏━┛
+              ┃    ┗━━━┓
+              ┃  神兽保佑 ┣┓
+              ┃  永无BUG！ ┏┛
+              ┗┓┓┏━┳┓┏┛
+               ┃┫┫ ┃┫┫
+               ┗┻┛ ┗┻┛
+
+Pycharm多行注释解注释：按快捷键Ctrl + /
+Pycharm多行注释解注释：按快捷键Ctrl + /
+Pycharm多行注释解注释：按快捷键Ctrl + /
+
+"""
+"""
+    to implement the Lagrange-interpolation
+"""
+
+import time
+
+
+
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Digital_Analysis_Learning
 
-
+数字分析教程中的一些算法的代码实现
